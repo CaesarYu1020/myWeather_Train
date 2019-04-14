152d427f97044a5f989c286e94688af91077ed59
CaesarYu1020 <bluesea861020@gmail.com>
2019-04-14 17:51:55 +0800

    debug

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from mTrain import myTrain
 
 run=True
 while(run):
-    print('**********************')
-    print('請選擇你的功能 W:台北/基隆天氣週報 T:火車、鬧鐘時刻 Q:離開程式')
-    print('**********************')
-    func=input('選擇 : ')
-    if func=='w' or func=="W":
-        HowsWeather('台北')
-        HowsWeather('基隆')
-    if func=='t' or func=="T":
-        myTrain()
-    if func == 'q' or func == "Q":
-        run=False
+    try:
+        print('**********************')
+        print('請選擇你的功能 W:台北/基隆天氣週報 T:火車、鬧鐘時刻 Q:離開程式')
+        print('**********************')
+        func=input('選擇 : ')
+        if func=='w' or func=="W":
+            HowsWeather('台北')
+            HowsWeather('基隆')
+        if func=='t' or func=="T":
+            myTrain()
+        if func == 'q' or func == "Q":
+            run=False
+    except:
+        print("Something's Wrong! Restart this program!!")
+        run = False
